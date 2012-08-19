d73bfe121a83b80f7bfece9c4c98506f9b0a6d09
Leo Franchi <lfranchi@kde.org>
2012-08-19 12:18:02 -0400

    mysyte

--- syte/syte_settings.py
+++ syte/syte_settings.py
@@ -5,16 +5,13 @@ COMPRESS_REVISION_NUMBER = '1.0'
 
 
 #Blog Integration: Tumblr
-TUMBLR_BLOG_URL = '[ENTER TUMBLR BLOG URL] ex. rigoneri.tumblr.com'
-TUMBLR_API_URL = 'http://api.tumblr.com/v2/blog/{0}'.format(TUMBLR_BLOG_URL)
-TUMBLR_API_KEY = '[ENTER TUMBLR API KEY HERE, SEE TUMBLR SETUP INSTRUCTIONS]'
-
 #RSS Feed Integration: (by default use Tumbrl rss feed)
 RSS_FEED_ENABLED = True
 RSS_FEED_URL = 'http://{0}/rss'.format(TUMBLR_BLOG_URL)
 
 #Twitter Integration
 TWITTER_INTEGRATION_ENABLED = True
+<<<<<<< HEAD
 TWITTER_API_URL = 'https://api.twitter.com/'
 TWITTER_CONSUMER_KEY = '[ENTER TWITTER CONSUMER KEY HERE, SEE TWITTER SETUP INSTRUCTIONS]'
 TWITTER_CONSUMER_SECRET = '[ENTER TWITTER CONSUMER SECRET HERE, SEE TWITTER SETUP INSTRUCTIONS]'
@@ -35,12 +32,12 @@ GITHUB_OAUTH_ACCESS_TOKEN_URL = 'https://github.com/login/oauth/access_token'
 
 
 #Dribbble Integration
-DRIBBBLE_INTEGRATION_ENABLED = True
+DRIBBBLE_INTEGRATION_ENABLED = False
 DRIBBBLE_API_URL = 'http://api.dribbble.com/players/'
 
 
 #Instagram Integration
-INSTAGRAM_INTEGRATION_ENABLED = True
+INSTAGRAM_INTEGRATION_ENABLED = False
 INSTAGRAM_API_URL = 'https://api.instagram.com/v1/'
 INSTAGRAM_ACCESS_TOKEN = '[ENTER INSTAGRAM ACCESS TOKEN HERE, SEE INSTAGRAM SETUP INSTRUCTIONS]'
 INSTAGRAM_USER_ID = '[ENTER INSTAGRAM USER_ID HERE, SEE INSTAGRAM SETUP INSTRUCTIONS]'
@@ -102,6 +99,6 @@ if DEPLOYMENT_MODE == 'dev':
     DEBUG = True
 else:
     DEBUG = False
-    SITE_ROOT_URI = '[ENTER PROD URL HERE] ex. http://rigoneri.herokuapp.com/'
+    SITE_ROOT_URI = 'http://www.lfranchi.com/'
 
 MEDIA_URL = SITE_ROOT_URI + 'static/'
